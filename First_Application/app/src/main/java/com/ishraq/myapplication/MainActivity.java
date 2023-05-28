b0f5adea2aa7de5c51852bd65cf2b8094d217869
ahmedishraq <ishraqahmedbd@gmail.com>
2023-05-29 04:12:51 +0600

    design spec learn

--- First_Application/app/src/main/java/com/ishraq/myapplication/MainActivity.java
+++ First_Application/app/src/main/java/com/ishraq/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.ishraq.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button do_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        do_button = findViewById(R.id.domagic);
+
+        do_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                do_button.setBackgroundColor(Color.BLACK);
+            }
+        });
     }
 }
